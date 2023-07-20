7d7a37831c7f41b09f9102b722a635661e2d0276
LJY <complete0415@naver.com>
2023-07-20 23:46:12 +0900

    [이지영]

--- src/main/java/com/javaba/mixologyconnect/member/model/dao/MemberDAO.java
+++ src/main/java/com/javaba/mixologyconnect/member/model/dao/MemberDAO.java
@@ -1,10 +1,15 @@
 package com.javaba.mixologyconnect.member.model.dao;
 
+import static com.javaba.mixologyconnect.common.JDBCTemplate.*;
 import java.io.FileInputStream;
+import java.nio.file.ClosedDirectoryStreamException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
+
+import com.javaba.mixologyconnect.member.model.vo.Member;
 
 
 public class MemberDAO {
@@ -29,5 +34,35 @@ public class MemberDAO {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**@author 이지영
+	 * 회원가입 DAO
+	 * @param conn
+	 * @param mem
+	 * @return result
+	 * @throws Exception
+	 */
+	public int signUp(Connection conn, Member mem)throws Exception {
+		int result = 0;
+		
+		try {
+			String sql =prop.getProperty("signUp");
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, mem.getMemberName());
+			pstmt.setString(2, mem.getMemberId());
+			pstmt.setString(3, mem.getMemberPw());
+			pstmt.setString(4, mem.getMemberTel());
+			pstmt.setString(5, mem.getMemberEmail());
+			pstmt.setString(6, mem.getMemberAddress());
+			
+			result = pstmt.executeUpdate();
+			
+		}finally {
+			close(pstmt);	
+		}
+		return result;
 	}
 }
