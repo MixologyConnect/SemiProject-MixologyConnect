bd001913a865c300f5a5a666f46aab3abce02a80
seohyun <dhtjgus1356@naver.com>
2023-07-21 17:50:04 +0900

    오서현/ 게시글 목록에 기능 넣기 ing...

--- src/main/java/com/javaba/mixologyconnect/board/controller/boardAllServlet.java
+++ src/main/java/com/javaba/mixologyconnect/board/controller/boardAllServlet.java
@@ -1,6 +1,7 @@
 package com.javaba.mixologyconnect.board.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.javaba.mixologyconnect.board.model.service.BoardService;
 
 @WebServlet("/board/boardAll")
 public class boardAllServlet extends HttpServlet {
@@ -18,6 +21,20 @@ public class boardAllServlet extends HttpServlet {
 		
 		try {
 			String path = "/WEB-INF/views/board/boardAll.jsp";
+			
+			int type = Integer.parseInt(req.getParameter("no"));
+			
+			int cp = 1;
+			
+			if(req.getParameter("cp") != null) {
+				Integer.parseInt(req.getParameter("cp"));
+			}
+			
+			BoardService service = new BoardService();
+			
+			Map<String, Object> map = service.selectBoardAll(type, cp);
+			req.setAttribute("map", map);
+			
 			
 			RequestDispatcher dispatcher = req.getRequestDispatcher(path);
 			dispatcher.forward(req, resp);
