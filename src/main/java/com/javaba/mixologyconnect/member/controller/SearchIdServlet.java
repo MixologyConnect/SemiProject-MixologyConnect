7aad7e9ad7a2f6aee017eeb1ab6a856b7a22089a
mirae <mslcc458@naver.com>
2023-07-25 19:26:18 +0900

    아이디 찾기

--- src/main/java/com/javaba/mixologyconnect/member/controller/SearchIdServlet.java
+++ src/main/java/com/javaba/mixologyconnect/member/controller/SearchIdServlet.java
@@ -19,20 +19,11 @@ public class SearchIdServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-
-		String path ="/WEB-INF/views/member/searchId.jsp";
-
-		req.getRequestDispatcher(path).forward(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 		// 파라미터 얻어오기
 		String memberName = req.getParameter("memberName");
 		String memberTel = req.getParameter("memberTel");
 		
-		
+
 		try {
 			
 			MemberService service = new MemberService();
@@ -42,13 +33,24 @@ public class SearchIdServlet extends HttpServlet{
 			new Gson().toJson(member, resp.getWriter());
 			
 			
+			
+			String path ="/WEB-INF/views/member/searchId.jsp";
+			
+			req.getRequestDispatcher(path).forward(req, resp);
+			
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
 		
 		
-		
+		doGet(req, resp); // POST로 전달된 요청을 doGet()으로 전달하여 수행
 		
 		
 		
