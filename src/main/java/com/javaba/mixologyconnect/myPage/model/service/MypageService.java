f44abca02847a76fc1c45bba120ef87919d5c3db
cje <cje960215@naver.com>
2023-07-31 09:05:57 +0900

    no message

---
+++ src/main/java/com/javaba/mixologyconnect/myPage/model/service/MypageService.java
@@ -0,0 +1,17 @@
+package com.javaba.mixologyconnect.myPage.model.service;
+
+import java.util.Map;
+
+import com.javaba.mixologyconnect.member.model.vo.Member;
+import com.javaba.mixologyconnect.myPage.model.dao.MypageDAO;
+
+public class MypageService {
+	
+	MypageDAO dao = new MypageDAO();
+
+	public Map<String, Object> selectMypageList(int cp, Member loginMember) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
