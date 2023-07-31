5f14f19286b38e688e1246c6dabbc62e26a0ce39
SeongSuIm <eric0695@naver.com>
2023-07-31 14:48:41 +0900

    dd

--- src/main/webapp/resources/js/manager.js
+++ src/main/webapp/resources/js/manager.js
@@ -12,3 +12,48 @@ function selectMember(){
 
     result.innerHTML = str;
 }
+
+document.getElementById("member-btn").addEventListener("click", function(){
+
+    const input = document.getElementById("searchMember");
+    const table = document.getElementById("resultMember");
+    
+    
+    $.ajax({
+        url : "manager/manger",
+        data : {"memberId" : input.value},
+        type : "POST",
+        dataType : "JSON",
+        
+        success : function(member){
+
+            console.log("member");
+
+            if(member != null){
+
+
+            
+                var htmls = "<td>" + "<input type='checkbox' name='chk1'>" +"</td>" +
+                            "<td>" + member.memberNo +"</td>" +
+                            "<td>" + member.memberId +"</td>" +
+                            "<td>" + member.memberName +"</td>" +
+                            "<td>" + member.memberTel +"</td>" +
+                            "<td>" + member.memberAddress +"</td>" +
+                            "<td>" + member.secessionFlag +"</td>" + "</tr>";
+                table.append(htmls);
+                
+            }
+        },
+        error : function(request,staus,error){
+            console.log("에러 발생");
+
+            console.log("상태코드 : " + request.status);
+
+            console.log(request.responseText);
+
+            console.log(error);
+
+        }
+    })
+
+});
