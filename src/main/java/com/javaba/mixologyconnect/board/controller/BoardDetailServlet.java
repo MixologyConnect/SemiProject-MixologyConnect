54f9e8e1e0d8ddd8c9b8b1c9b1b71d03ebb4b342
seohyun <dhtjgus1356@naver.com>
2023-08-01 10:37:38 +0900

    오서현 / 점메추

--- src/main/java/com/javaba/mixologyconnect/board/controller/BoardDetailServlet.java
+++ src/main/java/com/javaba/mixologyconnect/board/controller/BoardDetailServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.javaba.mixologyconnect.board.model.service.BoardService;
 import com.javaba.mixologyconnect.board.model.service.ReplyService;
 import com.javaba.mixologyconnect.board.model.vo.BoardDetail;
+import com.javaba.mixologyconnect.board.model.vo.BoardImage;
 import com.javaba.mixologyconnect.board.model.vo.Reply;
 
 
@@ -33,10 +34,21 @@ public class BoardDetailServlet extends HttpServlet {
 		
 		BoardDetail detail = service.selectBoardDetail(boardNo);
 		
+		List<BoardImage> ImageList = detail.getImageList();
 		
 		if(detail != null) {
 			List<Reply> rList = new ReplyService().selectReplyList(boardNo);
 			req.setAttribute("rList", rList);
+		}
+		
+		if(detail.getImageList() != null) {
+			
+			for( BoardImage imageList : ImageList) {
+				
+				System.out.println("imageList : " +  imageList);
+				
+				
+			}
 		}
 		
 		req.setAttribute("detail", detail);
@@ -55,4 +67,28 @@ public class BoardDetailServlet extends HttpServlet {
 	
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		try {
+
+			int boardNo = Integer.parseInt(req.getParameter("boardNo"));
+			
+			BoardService service = new BoardService();
+			
+			List<BoardImage> imageList = service.selectImage(boardNo);
+			
+			System.out.println("imageList : " +  imageList);
+			
+			resp.getWriter().print(imageList);			
+		
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+		
+	}
 }
--- src/main/webapp/resources/js/boardAll.js
+++ src/main/webapp/resources/js/boardAll.js
@@ -15,21 +15,3 @@ $.ajax({
 
 
 
-$.ajax({
-
-url : "/board/boardAll",
-
-data : {"boardNo" : boardNo},
-
-type : "POST",
-
-
-success : function (imageList){
-  console.log(imageList)
-
-},
-error: function () {
-  console.log("에러 발생");
-}
-
-})
